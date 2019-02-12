db5c6fb4a0b1bc5f073074a41a0f21db1d415576
scott evans <scottanthonyevans@hotmail.com>
2019-02-12 15:21:06 +0000

    Exercise19 or Section2 Exe on Vidly site - added views for Customers and Movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -34,23 +34,40 @@ namespace Vidly.Controllers
             //return RedirectToAction("Index", "Home", new {page = 1, sortBy = Name);
 
         }
+
         public ActionResult Edit(int id)
         {
             return Content("id=" + id);
         }
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
 
-            return Content(String.Format("pageIndex={0}&sortBy ={1}", pageIndex, sortBy));
-        }
+        //    return Content(String.Format("pageIndex={0}&sortBy ={1}", pageIndex, sortBy));
+        //}
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
+        }
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Name = "Shrek", Id = 1},
+                new Movie {Name = "Wall-e", Id = 2}
+            };
         }
     }
 }
