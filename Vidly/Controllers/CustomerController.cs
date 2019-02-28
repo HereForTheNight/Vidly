21c7d5c6001041f3a2abc1dc4a05a1456236a282
scott evans <scottanthonyevans@hotmail.com>
2019-02-28 16:31:50 +0000

    Build Before Exercise 3

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -4,35 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
         // GET: Customer
+        private MyDBContext _context;
+
+        public CustomerController()
+        {
+            _context = new MyDBContext();
+
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
             return View(customer);
         }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
-        }
-
+ 
     }
 }
